1ca511913be38b22236627f9e95cfd7d103f020c
Tom Handegård <tomhandegard@gmail.com>
2021-11-27 15:14:52 +0100

    Added more examples

---
+++ ch5/structparam.go
@@ -0,0 +1,23 @@
+package main
+
+type MyFuncOpts struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func MyFunc(opts MyFuncOpts) error {
+	// do something here
+	return nil
+}
+
+func main() {
+	MyFunc(MyFuncOpts{
+		LastName: "Patel",
+		Age:      50,
+	})
+	MyFunc(MyFuncOpts{
+		FirstName: "Joe",
+		LastName:  "Smith",
+	})
+}
